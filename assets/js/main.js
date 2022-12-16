e59bad1744286409a3d401a7c295f6a9e30c5cba
Alina-Altayeva <alina.aa764@gmail.com>
2022-12-16 21:23:08 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $('.bi-heart-fill').on('click', function () {
+    $(this).toggleClass('red-like')
+  })
+  var header = $('.arrow-top')
+  var scrollChange = 10;
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= scrollChange) {
+      header.addClass('arrow-show');
+    } else {
+      header.removeClass("arrow-show");
+    }
+  });
+})
